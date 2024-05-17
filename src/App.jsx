ddce188127982ed65a20076a24e0d2ac15112e59
Daniel <111338107+vlnt-trsv@users.noreply.github.com>
2024-05-18 02:56:27 +0700

    update file

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Auth from "./modules/auth/Auth";
 import Admin from "./modules/admin/Admin";
 import Profile from "./modules/profile/Profile";
-import Layout from "./modules/Layout/Layout";
+import Layout from "./modules/layout/Layout";
 import NewRequest from "./modules/profile/Request/NewRequest";
 import User from "./modules/profile/User/User";
 
